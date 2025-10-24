50d7eb9e656f8a2fa18bee5428e6a15315eb357a
msalafudin <muhammadsalafudin@hotmail.com>
2025-10-24 08:51:14 +0700

    C

--- resources/views/main-page/layouts/navbar.blade.php
+++ resources/views/main-page/layouts/navbar.blade.php
@@ -6,11 +6,11 @@
         <div class="row align-items-center">
             <div class="col-lg-2">
                 <div class="logo cursor-scale small">
-                    <a class="logo_img" href="index.html" title="mediic">
+                    <a class="logo_img" href="/" title="rsi2kendal">
                         <img src="assets/images/logo.png" alt="logo" />
                     </a>
-                    <a class="main_sticky" href="index.html" title="mediic">
-                        <img src="assets/images/logo2.png" alt="logo" />
+                    <a class="main_sticky" href="/" title="rsi2kendal">
+                        <img src="assets/images/logo.png" alt="logo" />
                     </a>
                 </div>
             </div>
@@ -98,45 +98,13 @@
         <nav class="mediic_menu">
             <ul class="nav_scroll">
                 <li>
-                    <a class="mdy-hover cursor-scale small" href="#">Beranda </a>
-                </li>
-                <li><a class="mdy-hover" href="about.html">About</a></li>
-                <li>
-                    <a class="mdy-hover" href="#">Tentang Kami </a>
-                    <ul class="sub-menu">
-                        <li><a href="about.html">Informasi Umum</a></li>
-                        <li><a href="service.html">Sejarah</a></li>
-                        <li><a href="team.html">Visi & Misi</a></li>
-                        <li><a href="team-details.html">Tim Kami</a></li>
-                        <li><a href="project.html">Indikator Mutu</a></li>
-                    </ul>
+                    <a class="mdy-hover cursor-scale small" href="/">Beranda </a>
                 </li>
                 <li>
-                    <a class="mdy-hover" href="#">Layanan </a>
-                    <ul class="sub-menu">
-                        <li><a href="about.html">Dokter</a></li>
-                        <li><a href="service.html">Poliklinik</a></li>
-                        <li><a href="team.html">Jadwal</a></li>
-                        <li><a href="team-details.html">Rawat Inap</a></li>
-                        <li><a href="project.html">Pelayanan Unit Khusus</a></li>
-                        <li><a href="project.html">Pelayanan Penunjang</a></li>
-                        <li><a href="project.html">Fasilitas Umum</a></li>
-                        <li><a href="project.html">Medical Check Up</a></li>
-                    </ul>
+                    <a class="mdy-hover cursor-scale small" href="/kamar-online">Ketersediaan Kamar</a>
                 </li>
                 <li>
-                    <a class="mdy-hover cursor-scale small" href="#">Berita </a>
-                </li>
-                <li>
-                    <a class="mdy-hover cursor-scale small" href="#">Pendaftaran </a>
-                </li>
-                <li>
-                    <a class="mdy-hover cursor-scale small" href="contact.html">Hubungi Kami</a>
-                    <ul class="sub-menu">
-                        <li><a href="about.html">Kontak Kami</a></li>
-                        <li><a href="service.html">Kritik & Saran</a></li>
-                        <li><a href="team.html">Survey Kepuasan</a></li>
-                    </ul>
+                    <a class="mdy-hover cursor-scale small" href="/indikatormutu">Indikator Mutu</a>
                 </li>
             </ul>
         </nav>
